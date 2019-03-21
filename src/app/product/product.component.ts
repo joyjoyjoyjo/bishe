6b519d4ff2f15be1fd81916496518f1f5c1b646a
xiaoyi <xiaoyishikongde@163.com>
2019-03-21 18:09:36 +0800

    product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
+  private Products:Array<Product>;
   constructor() { }
 
   ngOnInit() {
+    this.Products=[
+   new Product(1,"第一个",1.99,3.5,"这是一个商品描述",["电子产品","硬件设备"]),
+   new Product(1,"第一个",1.99,3.5,"这是一个商品描述",["电子产品","硬件设备"]),
+   new Product(1,"第一个",1.99,3.5,"这是一个商品描述",["电子产品","硬件设备"]),
+   new Product(1,"第一个",1.99,3.5,"这是一个商品描述",["电子产品","硬件设备"]),
+   new Product(1,"第一个",1.99,3.5,"这是一个商品描述",["电子产品","硬件设备"]),
+   new Product(1,"第一个",1.99,3.5,"这是一个商品描述",["电子产品","硬件设备"])
+ ]   
   }
 
+}
+
+export class Product{
+  constructor(
+    public id:number,
+    public title:string,
+    public price:number,
+    public rating:number,
+    public desc:string,
+    public categories:Array<string>
+  ){
+
+  }
 }
